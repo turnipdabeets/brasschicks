52c115530d79975d2eeabe058d5036bddbbe540c
Anna Garcia <annaegarcia@gmail.com>
2018-05-02 19:45:34 -0400

    add Oswald Google Font

--- plane/header.php
+++ plane/header.php
@@ -15,6 +15,7 @@
 <title><?php wp_title( '-', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
